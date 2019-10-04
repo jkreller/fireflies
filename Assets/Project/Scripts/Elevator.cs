fc17d21570e9de1b0841e63f9793a17d9c6c6cce
Julian Kreller <jkreller@eos-uptrade.de>
2019-10-04 18:24:23 +0200

    * Add factory, room, elevator * Add elevator function

---
+++ Assets/Project/Scripts/Elevator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    // Options
+    private float playerOffsetZ = 1;
+
+    // References
+    private Animator animator;
+    private Player player;
+
+    // Logic fields
+    private bool attachPlayer;
+    private float playerOffsetY;
+
+    void Awake()
+    {
+        animator = GetComponentInParent<Animator>();
+    }
+
+    void Update()
+    {
+        if (attachPlayer)
+        {
+            Vector3 playerPos = player.transform.position;
+            player.transform.position = new Vector3(playerPos.x, transform.position.y + playerOffsetY, playerPos.z);
+        }
+    }
+
+    public void OpenDoors(Player player)
+    {
+        SetAnimState(1);
+        this.player = player;
+    }
+
+    public void FinishOpening()
+    {
+        player.StartMovement(new Vector3(transform.position.x, player.transform.position.y, transform.position.z + playerOffsetZ), AfterPlayerMovement);
+    }
+
+    private void AfterPlayerMovement()
+    {
+        SetAnimState(2);
+        playerOffsetY = player.transform.position.y - transform.position.y;
+        attachPlayer = true;
+    }
+
+    private void SetAnimState(int state)
+    {
+        animator.SetInteger("AnimState", state);
+    }
+}
---
+++ Assets/Project/Scripts/Player.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Script for handling player input
+public class Player : MonoBehaviour
+{
+    // Options
+    [SerializeField] private float speed = 2;
+    [SerializeField] private float movementSpeed = 1;
+
+    // Logic fields
+    private float rotationX;
+    private float rotationY;
+    private bool isMoving;
+    private Vector3 targetPosition;
+    private MovingCallBack movingCallBack;
+
+    public delegate void MovingCallBack();
+
+    void Update()
+    {
+        // On left mouse click
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                switch (hit.transform.tag)
+                {
+                    case "Fireflies":
+                        Fireflies firefliesHitted = hit.transform.GetComponent<Fireflies>();
+                        firefliesHitted.Seperate(transform.position);
+                        break;
+                    case "Elevator":
+                        Elevator elevator = hit.transform.GetComponentInParent<Elevator>();
+                        elevator.OpenDoors(this);
+                        break;
+                }
+            }
+        }
+
+        // On moving mouse
+        rotationX += speed * Input.GetAxis("Mouse X");
+        rotationY -= speed * Input.GetAxis("Mouse Y");
+        transform.eulerAngles = new Vector3(rotationY, rotationX, 0);
+
+        // Move player if isset
+        if (isMoving)
+        {
+            Move();
+        }
+    }
+
+    public void StartMovement(Vector3 targetPos, MovingCallBack movingCallBack = null)
+    {
+        targetPosition = targetPos;
+        isMoving = true;
+        this.movingCallBack = movingCallBack;
+    }
+
+    private void Move()
+    {
+        if (Vector3.SqrMagnitude(transform.position - targetPosition) > 0.01)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPosition, movementSpeed * Time.deltaTime);
+        } else
+        {
+            isMoving = false;
+            if (movingCallBack != null)
+            {
+                movingCallBack();
+                movingCallBack = null;
+            }
+        }
+    }
+}
