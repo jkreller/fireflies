7f51a3b5ba965cb085543fc8b2e18e3604621eee
jkreller <33465273+jkreller@users.noreply.github.com>
2019-10-25 00:03:28 +0200

    Add glowing to controllers when attractingfireflies

--- Assets/Project/Scripts/FireFliesAttract.cs
+++ Assets/Project/Scripts/FireFliesAttract.cs
@@ -9,14 +9,21 @@ public class FireFliesAttract : ControllerComponent
     // References
     private Fireflies fireflies;
     [SerializeField] private SteamVR_Action_Boolean grabPinchAction;
+    [SerializeField] private Material glowingMaterial;
+    private Light lightObject;
+    private MeshRenderer[] meshRenderers;
+    private Material[] oldMaterials;
 
     // Logic fields
     [System.NonSerialized] public bool moveWithMouse;
     [System.NonSerialized] public bool shouldFollow;
+    private bool isGlowing;
 
     private void Awake()
     {
         fireflies = GameObject.Find("Fireflies").GetComponent<Fireflies>();
+        lightObject = GetComponentInChildren<Light>();
+        lightObject.enabled = false;
     }
 
     void Update()
@@ -26,9 +33,17 @@ public class FireFliesAttract : ControllerComponent
             if (grabPinchAction.GetStateDown(handType))
             {
                 shouldFollow = true;
+                if (!isGlowing)
+                {
+                    Glow(true);
+                }
             } else if (grabPinchAction.GetLastStateUp(handType))
             {
                 shouldFollow = false;
+                if (isGlowing)
+                {
+                    Glow(false);
+                }
             }
         } else if (moveWithMouse)
         {
@@ -38,6 +53,36 @@ public class FireFliesAttract : ControllerComponent
         } else
         {
             shouldFollow = false;
+        }
+    }
+
+    private void Glow(bool turnOn)
+    {
+        if (turnOn)
+        {
+            if (meshRenderers == null)
+            {
+                meshRenderers = GetComponentsInChildren<MeshRenderer>();
+                oldMaterials = new Material[meshRenderers.Length];
+                for (int i = 0; i < meshRenderers.Length; i++)
+                {
+                    oldMaterials[i] = meshRenderers[i].material;
+                }
+            }
+            foreach (MeshRenderer meshRenderer in meshRenderers)
+            {
+                meshRenderer.material = glowingMaterial;
+            }
+            lightObject.enabled = true;
+            isGlowing = true;
+        } else
+        {
+            for (int i = 0; i < meshRenderers.Length; i++)
+            {
+                meshRenderers[i].material = oldMaterials[i];
+            }
+            lightObject.enabled = false;
+            isGlowing = false;
         }
     }
 }
