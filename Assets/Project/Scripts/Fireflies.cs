1adbf83c9a12131d22f894e0fa811a4ef4b25b5d
bzeitschel <bzeitschel@eos-uptrade.de>
2019-10-28 12:15:17 +0100

    a lot of prefabs, cages work ...

--- Assets/Project/Scripts/Fireflies.cs
+++ Assets/Project/Scripts/Fireflies.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 // Script for fireflies particle system
 public class Fireflies : Movable
 {
+    
     // Options
     [Header("Fireflies Options")]
     [SerializeField] private bool initializeToLamp = true;
+    public int firefliesSize;
 
     // References
     private Animator animator;
@@ -16,13 +18,44 @@ public class Fireflies : Movable
     private float seperationDistance;
     private static float minScale; // one fourth of initial size, for example 0.5 if initial scale is 2
     private bool isInitialFireflies;
-
+    public bool deactivateFirefly;
+    public bool flyToNextPoint = false;
+    private List<GameObject> pathMakingObejcts;
 
     //Fireflies follow
     FireFliesAttract fireFliesAttract;
 
+    new void Update()
+    {
+        base.Update();
+        //Debug.Log(this.gameObject.transform.localScale);
+
+
+        if (this.gameObject.transform.localScale.x < 0.6 && this.gameObject.transform.localScale.x > 0.16)
+        {
+            firefliesSize = 2;
+        }
+        else if(this.gameObject.transform.localScale.x > 0.4)
+        {
+            firefliesSize = 1;
+        }
+        else if (this.gameObject.transform.localScale.x < 0.2)
+        {
+            firefliesSize = 4;
+        }
+        if(deactivateFirefly){
+            foreach (Collider c in GetComponents<Collider>())
+            {
+                c.enabled = false;
+            }
+        }
+
+    }
+
     void Start()
     {
+        pathMakingObejcts = new List<GameObject>();
+
         float avrgScale = (transform.localScale.x + transform.localScale.y + transform.localScale.z) / 3;
 
         animator = GetComponent<Animator>();
@@ -63,50 +96,100 @@ public class Fireflies : Movable
 
     private void OnTriggerEnter(Collider other)
     {
-        // If is colliding with another fireflies object
-        if (other.gameObject.CompareTag("Fireflies"))
+        if (!deactivateFirefly)
         {
-            if (FirefliesMerger.Instance.RequestMerging())
+            // If is colliding with another fireflies object
+            if (other.gameObject.CompareTag("Fireflies"))
             {
-                Merge(other.gameObject);
+                if (FirefliesMerger.Instance.RequestMerging())
+                {
+                    Merge(other.gameObject);
+                }
+            }
+            // go into cage1
+            if (other.gameObject.CompareTag("cage1"))
+                {
+                if (firefliesSize == 2)
+                {
+                    pathMakingObejcts.Add(GameObject.Find("cage1point1"));
+                    pathMakingObejcts.Add(GameObject.Find("cage1point2"));
+                    pathMakingObejcts.Add(GameObject.Find("cage1point3"));
+                    pathFinding(pathMakingObejcts);
+                    cageDoor cageDoor1 = GameObject.Find("CageDoor1").GetComponent<cageDoor>();
+                    cageDoor1.closeCage = true;
+                }
+               }
+            // go into cage2
+            if (other.gameObject.CompareTag("cage2"))
+            {
+                if (firefliesSize == 2)
+                {
+                    pathMakingObejcts.Add(GameObject.Find("cage2point1"));
+                    pathMakingObejcts.Add(GameObject.Find("cage2point2"));
+                    pathMakingObejcts.Add(GameObject.Find("cage2point3"));
+                    pathFinding(pathMakingObejcts);
+                    cageDoor cageDoor2 = GameObject.Find("CageDoor2").GetComponent<cageDoor>();
+                    cageDoor2.closeCage = true;
+                }
             }
         }
     }
+    private void pathFinding(List<GameObject> pathBubbles){
+        
+        foreach(GameObject bubble in pathBubbles){
+            StartMovement(bubble.transform.position);
+            StartCoroutine(WaitforPoint());
+            flyToNextPoint = false;
+            Destroy(bubble);
+        }
+        deactivateFirefly = true;
+    }
 
     void OnTriggerStay(Collider other){
-        // follow attractBall
-        if (other.gameObject.CompareTag("AttractBall"))
+        if (!deactivateFirefly)
         {
-            if (fireFliesAttract.follow)
+            // follow attractBall
+            if (other.gameObject.CompareTag("AttractBall"))
             {
-                if (Input.GetKey("f"))
+                if (fireFliesAttract.follow)
                 {
-                    StartMovement(other.gameObject.transform.position);
+                    if (Input.GetKey("f"))
+                    {
+                        StartMovement(other.gameObject.transform.position);
+                    }
                 }
             }
         }
     }
 
+    IEnumerator WaitforPoint()
+    {
+        yield return new WaitUntil(() => flyToNextPoint == true);
+    }
+
     // Seperate one fireflies object into two objects of half size
     public void Seperate(Vector3 playerPosition)
     {
-        // Check if is big enough to seperate
-        if ((transform.localScale * 0.5f).sqrMagnitude >= (new Vector3(minScale, minScale, minScale)).sqrMagnitude)
+        if (!deactivateFirefly)
         {
-            Vector3 normalizedDirection = (playerPosition - transform.position).normalized;
+            // Check if is big enough to seperate
+            if ((transform.localScale * 0.5f).sqrMagnitude >= (new Vector3(minScale, minScale, minScale)).sqrMagnitude)
+            {
+                Vector3 normalizedDirection = (playerPosition - transform.position).normalized;
 
-            Vector3 directionParts = Quaternion.AngleAxis(90, Vector3.up) * normalizedDirection * seperationDistance;
+                Vector3 directionParts = Quaternion.AngleAxis(90, Vector3.up) * normalizedDirection * seperationDistance;
 
-            Vector3 posPart1 = transform.position + directionParts;
-            Vector3 posPart2 = transform.position - directionParts;
+                Vector3 posPart1 = transform.position + directionParts;
+                Vector3 posPart2 = transform.position - directionParts;
 
-            GameObject partObject = gameObject;
-            partObject.transform.localScale *= 0.5f;
+                GameObject partObject = gameObject;
+                partObject.transform.localScale *= 0.5f;
 
-            Instantiate(partObject, posPart1, transform.rotation).GetComponent<Fireflies>();
-            Instantiate(partObject, posPart2, transform.rotation).GetComponent<Fireflies>();
+                Instantiate(partObject, posPart1, transform.rotation).GetComponent<Fireflies>();
+                Instantiate(partObject, posPart2, transform.rotation).GetComponent<Fireflies>();
 
-            Destroy(gameObject);
+                Destroy(gameObject);
+            }
         }
     }
 
@@ -119,14 +202,17 @@ public class Fireflies : Movable
     // Merge fireflies together with another one into one fireflies object with double size
     private void Merge(GameObject other)
     {
-        GameObject mergedObject = gameObject;
-        mergedObject.transform.position = transform.position + (other.transform.position - transform.position) * 0.5f;
-        mergedObject.transform.localScale = transform.localScale + other.transform.localScale;
+        if (!deactivateFirefly)
+        {
+            GameObject mergedObject = gameObject;
+            mergedObject.transform.position = transform.position + (other.transform.position - transform.position) * 0.5f;
+            mergedObject.transform.localScale = transform.localScale + other.transform.localScale;
 
-        Instantiate(mergedObject);
+            Instantiate(mergedObject);
 
-        Destroy(gameObject);
-        Destroy(other);
+            Destroy(gameObject);
+            Destroy(other);
+        }
     }
 
     private void SetAnimState(int state)
